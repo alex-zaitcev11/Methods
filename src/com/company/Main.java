62d6afe05ac98ccd4bd4daf53a3b8ffd6e4df472
alex-zaitcev11 <aleksandrzajcev697@gmail.com>
2022-06-23 12:45:13 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    static boolean dividesByTwo(int a){
+        return(a%2 == 0);
+    }
+    public static void main(String[] args){
+        System.out.println(dividesByTwo(6));
+    }
+}
+
+
+
